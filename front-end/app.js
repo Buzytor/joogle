18a71d4704a199a14592341b570dca0d4fe9676d
Jan Michalski <jasium.ready+github@gmail.com>
2014-10-25 14:45:09 +0200

    [F] Fix jade

--- front-end/app.js
+++ front-end/app.js
@@ -13,6 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.disable('view cache');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -21,11 +22,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.engine('jade', require('jade').__express);
-
 
 app.get('/', function(req, res) {
-    res.render('index', {});
+    res.render('index', {title: "Joogle"});
 });
 
 
