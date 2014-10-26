4a0e21850ce8673c6401ea58e306e125e6986b0f
Michał Błotniak <buoto@buzytor.com>
2014-10-26 05:04:15 +0100

    [E] Fix parser module return type

--- engine/lib/parser.js
+++ engine/lib/parser.js
@@ -2,7 +2,7 @@ var peg = require('pegjs');
 var ser = require('./serializer');
 var fs = require('fs');
 
-exports = function(req){
+module.exports  = function(req){
 		var parserCode = fs.readFileSync("parser.pegjs", "utf-8");
 		var parser = peg.buildParser(parserCode);
 		return ser.deserialize(parser.parse(req));
