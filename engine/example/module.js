1ee477ca3c1d5b170598201dd2fda527c67e28e2
Michał Błotniak <buoto@buzytor.com>
2014-10-26 08:43:54 +0100

    [E] Add new example functions, fix the old ones

--- engine/example/module.js
+++ engine/example/module.js
@@ -16,9 +16,49 @@ module.exports.addLength = function(a, b) {
 	return a.length + b.length;	
 };
 
+
 module.exports.addArray = function(a, b) {
+	var result = [];
 	for(var i = 0; i < a.length; i ++) {
-		a[i] += b[i];
+		result = a[i] + b[i];
+	}
+	return result;
+};
+
+module.exports.addArrayStrange = function(a, b) {
+	for(var i = 0; i < a.length; i ++) {
+		a[i] = a[i] + b[i];
 	}
 	return a;
 };
+var fill = module.exports.fill = function(a, b) { 
+	var result = [];
+	for(var i = 0; i < a; i++) { 
+		result[i] = b;	
+	}	
+	return result;
+};
+
+module.exports.lengths = function(a) {
+	var result = [];
+	for(var i = 0; i < a.length; i ++) {
+		result[i] = a[i].length;
+	}
+	return result;
+};
+
+module.exports.someStuff = function(a, b ,c) {
+	return [fill(a, b), fill(b, a), c, [6 , 3]];
+};
+
+module.exports.moreStuff = function(a, b) {
+	var ret = fill(a, b);
+	var x = ret.length;
+	var tab = [];
+	for(var i = 0; i < ret.length; i ++) {
+		tab.push(ret[i]);
+	}
+	var y  = tab.length;
+	return  ret.length + x + tab[0] + y;
+};
+
