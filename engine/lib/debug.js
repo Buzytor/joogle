20ef739c824a8a67d17e639158e22bebdfd0310f
Maciej Bielecki <zyla@prati.pl>
2014-10-26 05:02:31 +0100

    [E] Add debugging functions

---
+++ engine/lib/debug.js
@@ -0,0 +1,24 @@
+var types = require('./types');
+
+exports.printETs = function(ets) {
+	ets.forEach(exports.printET);
+};
+
+exports.printET = function(et) {
+	console.log('Expression type: ' + types.typeToString(et.type));
+	et.constraints.forEach(function(c) {
+		console.log('  ' + c.toString());
+	});
+};
+
+exports.printScope = function(scope) {
+	Object.keys(scope.vars).forEach(function(key) {
+		scope.vars[key].forEach(function(et) {
+			console.log(key + ' :: ' + types.typeToString(et.type));
+		});
+	});
+};
+
+exports.printCSet = function(cSet) {
+	console.log(cSet.join(', '));
+};
